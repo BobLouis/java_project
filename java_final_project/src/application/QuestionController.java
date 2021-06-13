be25300018b6ead65c2c5a7acda7b71985ac8b20
BobLouis <bob900316@gmail.com>
2021-06-13 21:32:35 +0800

    input array record as linkedlist

--- java_final_project/src/application/QuestionController.java
+++ java_final_project/src/application/QuestionController.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -27,6 +28,8 @@ public class QuestionController {
 	private CheckBox Cbox;
 	@FXML
 	private CheckBox Dbox;
+	@FXML
+	private Label    Caution;
 	
 	private Stage stage;
 	private Scene scene;
@@ -38,45 +41,70 @@ public class QuestionController {
 			//file io
 			{"An __________ allows data to flow into your program.","input stream","output stream","file name","all of the above"},
 			{"An __________ allows data to flow from your program.","input stream","output stream","file name","all of the above"},
-			{"The method __________ from the File class forces a physical write to the file of any data that is buffered.","close()","writeUTF()","flush()","writeObject()"},
-			{"The stream that is automatically available to your Java code is:","System.in","System.out","System.err","all of the above"},
+			{"The method __________ from the File class forces a \nphysical write to the file of any data that is buffered.","close()","writeUTF()","flush()","writeObject()"},
+			{"The stream that is automatically available to your \nJava code is:","System.in","System.out","System.err","all of the above"},
 			//javaFX
-			{"A JavaFX action event handler is an instance of _______.","ActionEvent","Action","EventHandler","EventHandler<ActionEvent>"},
-			{"A JavaFX action event handler contains a method ________.","public void actionPerformed(ActionEvent e)","public void action Performed(Event e)","public void handle(ActionEvent e)","public void handle(Event e)"},
-			{"A JavaFX event handler for event type T is an instance of _______.","ActionEvent","Action","EventHandler","EventHandler<T>"},
+			{"A JavaFX action event handler is an \ninstance of _______.","ActionEvent","Action","EventHandler","EventHandler<ActionEvent>"},
+			{"A JavaFX action event handler contains\n a method ________.","public void actionPerformed(ActionEvent e)","public void action Performed(Event e)","public void handle(ActionEvent e)","public void handle(Event e)"},
+			{"A JavaFX event handler for event type T\n is an instance of _______.","ActionEvent","Action","EventHandler","EventHandler<T>"},
 			//polymorphism
-			{"A class that uses an interface must use the keyword:","Extends","Inherits","Super","Implements"},
-			{"An interface and all of its methods headings are normally declared to be","public","private","protected","package access"},
+			{"A class that uses an interface must use\n the keyword:","Extends","Inherits","Super","Implements"},
+			{"An interface and all of its methods headings \nare normally declared to be","public","private","protected","package access"},
 			{"An interface may contain","instance variable","primitive variables","constant variables","all of the above"},
 			//inheritance
-			{"The class _______ is an ancestor class of all Java classes","String","Object","Math","JFrame"},
-			{"If an instance variable is not modified by public,protected or private then it is said to have:","package access","default access","friendly access","all of the above"},
+			{"The class _______ is an ancestor class of all \nJava classes","String","Object","Math","JFrame"},
+			{"If an instance variable is not modified by\n public,protected or private then it is said to have:","package access","default access","friendly access","all of the above"},
 			{"A super class is also called a","derived class","dominant class","sub class","base class"},
 			
 	};
 	
 	int[] answer = {1,2,3,4  ,4,3,4 ,4,1,3 ,2,2,4};
+	int inputOption = 0;
+	LinkedList<Integer> inputOptionArr = new LinkedList<>();
 	
 	private int Qall = answer.length;
 	private int Qcount = 0;
 	
+	private boolean isStart = true;
+	
 	
 	public void nextQuestion(ActionEvent event) throws IOException {
-		if(Qcount>=Qall) {
-			end(event);
+		//check if the option is selected
+		if(inputOption>0 || isStart==true) {
+			++Qcount;
+			//save the previous option
+			inputOptionArr.add(inputOption);
+			inputOption = 0;  //reset the inputOption 0-> no input
+			Caution.setText("");
+			if(Qcount>Qall) {
+				System.out.println(inputOptionArr);
+				end(event);
+			}
+			//reset all the option
+			Abox.setSelected(false);
+			Bbox.setSelected(false);
+			Cbox.setSelected(false);
+			Dbox.setSelected(false);
+
+			Qnumber.setText("Question "+(Qcount)+"/"+(Qall));
+			if(Qcount<Qall) {
+				Qarea.setText(question[Qcount-1][0]);
+				Abox.setText(question[Qcount-1][1]);
+				Bbox.setText(question[Qcount-1][2]);
+				Cbox.setText(question[Qcount-1][3]);
+				Dbox.setText(question[Qcount-1][4]);
+			}
+			isStart = false;
+		}else {
+			Caution.setText("Please choose a option");
+			System.out.println("Please choose a option");
 		}
-		Qnumber.setText("Question "+(++Qcount)+"/"+(Qall));
-		if(Qcount<Qall) {
-			Qarea.setText(question[Qcount-1][0]);
-			Abox.setText(question[Qcount-1][1]);
-			Bbox.setText(question[Qcount-1][2]);
-			Cbox.setText(question[Qcount-1][3]);
-			Dbox.setText(question[Qcount-1][4]);
-		}
+		
 	}
 	
 	private void end(ActionEvent event) throws IOException {
 		System.out.println("end");
+		
 		
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("End.fxml"));
 		root = loader.load();
@@ -92,6 +120,7 @@ public class QuestionController {
 	@FXML
 	private void handleAbox() {
 		if(Abox.isSelected()) {
+			inputOption=1;
 			Bbox.setSelected(false);
 			Cbox.setSelected(false);
 			Dbox.setSelected(false);
@@ -101,6 +130,7 @@ public class QuestionController {
 	@FXML
 	private void handleBbox() {
 		if(Bbox.isSelected()) {
+			inputOption=2;
 			Abox.setSelected(false);
 			Cbox.setSelected(false);
 			Dbox.setSelected(false);
@@ -110,6 +140,7 @@ public class QuestionController {
 	@FXML
 	private void handleCbox() {
 		if(Cbox.isSelected()) {
+			inputOption=3;
 			Bbox.setSelected(false);
 			Abox.setSelected(false);
 			Dbox.setSelected(false);
@@ -119,13 +150,11 @@ public class QuestionController {
 	@FXML
 	private void handleDbox() {
 		if(Dbox.isSelected()) {
+			inputOption=4;
 			Bbox.setSelected(false);
 			Cbox.setSelected(false);
 			Abox.setSelected(false);
 		}
 	}
 	
-	
-	
-
 }
